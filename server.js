9113cec8f04b549a8647f284d5bded255d3e997d
Jan Kaduch <kaduch05@gmail.com>
2026-02-05 14:06:08 +0100

    Setup: add nodemon depedency and create package-lock.json

---
+++ server.js
@@ -0,0 +1,13 @@
+import http from 'node:http';
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+
+
+    res.end('Hello, Worlds!');
+})
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
